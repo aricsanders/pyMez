01517c44fd39942c5d0673e56cf5ef73fb3bddae
aricsanders <aric.sanders@gmail.com>
2017-02-16 10:14:02 -0700

    Updated Documentation

--- Code/DataHandlers/__init__.py
+++ Code/DataHandlers/__init__.py
@@ -1,11 +1,21 @@
-"""The DataHandlers subpackage is designed to manipulate data, by allowing different data types to be opened,
+"""
+DataHandlers
+------------
+The DataHandlers subpackage is designed to manipulate data, by allowing different data types to be opened,
 created, saved and updated. The subpackage is further divided into modules grouped by a common theme. Classes for data
-that are already on disk normally follows the following pattern instance=ClassName(file_path,**options). For Example to
-open a XML file that you don't know the model, use xml=pyMeasure.Code.DataHandlers.XMLModels.XMLBase('MyXML.xml').
+that are already on disk normally follows the following pattern:
+`instance=ClassName(file_path,**options)`
+For Example to
+open a XML file that you don't know the model, use
+`xml=pyMeasure.Code.DataHandlers.XMLModels.XMLBase('MyXML.xml')'
+or
+`xml=XMLBase('MyXML.xml')`
 
 All data models normally have save(), str() and if appropriate show() methods.
 
-DataHandlers typically import from Utils but NOT from Analysis, InstrumentControl or FrontEnds
+Import Structure
+----------------
+DataHandlers typically import from Utils but __NOT__ from Analysis, InstrumentControl or FrontEnds
 
 """
 
